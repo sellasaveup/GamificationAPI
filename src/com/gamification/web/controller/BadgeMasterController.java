a7968ea76bba68b78e3587aa0c212fa58ff66069
Sellasaveup <sellasaveup@gmail.com>
2015-08-09 13:30:32 +0530

    conflict

--- src/com/gamification/web/controller/BadgeMasterController.java
+++ src/com/gamification/web/controller/BadgeMasterController.java
@@ -1,5 +1,6 @@
 package com.gamification.web.controller;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
@@ -9,6 +10,10 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
 import com.gamification.RequestTransformer;
 import com.gamification.web.manager.WebManager;

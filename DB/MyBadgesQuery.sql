6bd2d8831cae53c8052619f7d67f5ef0550e3f87
Sujatha Balaji <sellasaveup@users.noreply.github.com>
2015-08-20 15:01:20 +0530

    Update MyBadgesQuery.sql

--- DB/MyBadgesQuery.sql
+++ DB/MyBadgesQuery.sql
@@ -5,4 +5,6 @@ SELECT ub.BADGE_CODE FROM ss_tr_user_badge ub WHERE ub.USER_CODE='GBS03146' AND 
 select * from ss_ma_badge b where b.BADGE_CODE not in (
 SELECT ub.BADGE_CODE FROM ss_tr_user_badge ub WHERE ub.USER_CODE='GBS03146' AND ub.GOAL_CODE='HYPE_GOAL') and b.GOAL_CODE='HYPE_GOAL' and b.EXPIRY_DATE>sysdate()
 --getAllMyBadges
-SELECT ub.BADGE_CODE FROM ss_tr_user_badge ub WHERE ub.USER_CODE='GBS03146' AND ub.GOAL_CODE='HYPE_GOAL'
+SELECT ub.BADGE_CODE, b.NAME, b.IMAGE, b.STORY, ub.DATE FROM ss_tr_user_badge ub, ss_ma_badge b WHERE ub.USER_CODE='GBS03146' 
+AND ub.GOAL_CODE='HYPE_GOAL' and ub.GOAL_CODE=b.GOAL_CODE and b.BADGE_CODE=ub.BADGE_CODE
+

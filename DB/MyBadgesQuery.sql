9c071ce1f1db82f9c8a9dee9a73dc1e4b29f7f05
Sujatha Balaji <sellasaveup@users.noreply.github.com>
2015-08-20 15:09:15 +0530

    Update MyBadgesQuery.sql

--- DB/MyBadgesQuery.sql
+++ DB/MyBadgesQuery.sql
@@ -1,10 +1,6 @@
---getMyBadge
-select * from ss_ma_badge b where b.BADGE_CODE in (
-SELECT ub.BADGE_CODE FROM ss_tr_user_badge ub WHERE ub.USER_CODE='GBS03146' AND ub.GOAL_CODE='HYPE_GOAL') and b.GOAL_CODE='HYPE_GOAL' and b.EXPIRY_DATE>sysdate()
---getMyLockedBadge
-select * from ss_ma_badge b where b.BADGE_CODE not in (
-SELECT ub.BADGE_CODE FROM ss_tr_user_badge ub WHERE ub.USER_CODE='GBS03146' AND ub.GOAL_CODE='HYPE_GOAL') and b.GOAL_CODE='HYPE_GOAL' and b.EXPIRY_DATE>sysdate()
---getAllMyBadges
+--getMyBadges
 SELECT ub.BADGE_CODE, b.NAME, b.IMAGE, b.STORY, ub.DATE FROM ss_tr_user_badge ub, ss_ma_badge b WHERE ub.USER_CODE='GBS03146' 
-AND ub.GOAL_CODE='HYPE_GOAL' and ub.GOAL_CODE=b.GOAL_CODE and b.BADGE_CODE=ub.BADGE_CODE
-
+AND ub.GOAL_CODE='HYPE_GOAL' and ub.GOAL_CODE=b.GOAL_CODE and b.BADGE_CODE=ub.BADGE_CODE order by ub.DATE desc
+--getMyLockedBadge
+selectb.NAME, b.IMAGE, b.STORY from ss_ma_badge b where b.BADGE_CODE not in (
+SELECT ub.BADGE_CODE FROM ss_tr_user_badge ub WHERE ub.USER_CODE='GBS03146' AND ub.GOAL_CODE='HYPE_GOAL') and b.GOAL_CODE='HYPE_GOAL' and b.EXPIRY_DATE>sysdate()

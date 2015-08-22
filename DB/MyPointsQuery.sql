7f9e7d27f26121335271e7b34e3389d3f6428a07
Sujatha Balaji <sellasaveup@users.noreply.github.com>
2015-08-22 14:38:43 +0530

    Update MyPointsQuery.sql

--- DB/MyPointsQuery.sql
+++ DB/MyPointsQuery.sql
@@ -18,21 +18,18 @@ select @curRank := @curRank + 1 AS rank, inQuery.*  from
 --this month leaderboard
 
 select @curRank := @curRank + 1 AS rank, inQuery.*  from 
-(SELECT sum(ua.POINTS) as totalpoints, month(ua.DATE), ua.USER_CODE FROM ss_tr_user_action ua where 
+(SELECT sum(ua.POINTS) as totalpoints, month(ua.DATE), ua.USER_CODE, u.image, u.name FROM ss_tr_user_action ua, ss_ma_user u where u.user_code = ua.user_code and
  extract(year_month from ua.DATE)= 201507  
  group by ua.USER_CODE, month(ua.DATE)  
  order by totalpoints desc limit 10) as inQuery, (SELECT @curRank := 0) r
-
 
 
 --All Time leaderboard
 
 
 select @curRank := @curRank + 1 AS rank, inQuery.*  from 
-(SELECT sum(ua.POINTS) as totalPoints, ua.USER_CODE FROM ss_tr_user_action ua 
-	group by ua.USER_CODE order by totalPoints desc limit 10) as inQuery, (SELECT @curRank := 0) r
-
-
+(SELECT sum(ua.POINTS) as totalPoints, ua.USER_CODE, u.NAME, u.IMAGE  FROM ss_tr_user_action ua, ss_ma_user u
+ group by ua.USER_CODE order by totalPoints desc limit 10) as inQuery, (SELECT @curRank := 0) r
 
 -- MyPointsGraph
 SELECT sum(ua.POINTS) as totalpoints, monthname(ua.DATE) FROM ss_tr_user_action ua where ua.STATUS='Active'

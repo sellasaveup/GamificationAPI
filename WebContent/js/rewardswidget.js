359ddb4e3467026e11532dfe0aa80856f0cb8793
Sujatha Balaji <sujatha.b@sella.it>
2015-08-29 17:00:08 +0530

    No rewards Message update

--- WebContent/js/rewardswidget.js
+++ WebContent/js/rewardswidget.js
@@ -1,7 +1,7 @@
 function getMyRewards(divId, userCode, goalCode, commonUrl) {
 
 	var buildUrl =  commonUrl + 'GET_MY_REWARD?userCode='
-		buildUrl = buildUrl + userCode + '&goalCode=' + goalCode;	    
+		buildUrl = buildUrl + userCode + '&goalCode=' + goalCode;	
 	$.ajax({
 		type : "GET",
 		url : buildUrl,
@@ -9,18 +9,23 @@ function getMyRewards(divId, userCode, goalCode, commonUrl) {
 		contentType : "application/json; charset=UTF-8",
 		success : function(data) {
 			var rewardsHtml = '<div class="well"> <div class="row">';
-			var items = [];
-			var rewardList = data.Response;				
+			
+			var rewardList = data.Response;
+			if(!rewardList.isSuccess){
 			$.each( rewardList, function( index, obj ) {				
 				rewardsHtml =  rewardsHtml + '<div class="col-sm-4">' +
-				rewardList[index].name+ '<span class="badge">' + rewardList[index].reedemPoints + '</span> <br>' 
+				rewardList[index].name+ ' <span class="badge">' + rewardList[index].reedemPoints + ' pts</span> <br>' 
 				+ '<a href="redeemURL" class="thumbnail"> <p> ' +
 				rewardList[index].story + '</p>' + '<img src="./img/rewards/'+ rewardList[index].image + '" class="img-thumbnail" width="200" height="200"> </a>' +
 				'</div>';
 				 
-			 });			
+			 });	
 			rewardsHtml = rewardsHtml + '</div></div>';
 			 $("#"+divId).append(rewardsHtml);
+		}else{
+			rewardsHtml = '<div>' + 'You have not yet earned any points to reedem' + '</div>';
+			$("#"+divId).append(rewardsHtml);
+		}
 		},
 		error : function(e) {
 			console.log('rewards call failure : ' + e);

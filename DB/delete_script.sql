1406cff02bba3cf87fad8946b0b8807512d35e05
Sujatha Balaji <sujatha.b@sella.it>
2015-09-06 23:44:59 +0530

    clean up script

---
+++ DB/delete_script.sql
@@ -0,0 +1,16 @@
+
+drop TABLE  ss_tr_notification;
+drop TABLE  ss_tr_user_level;
+drop table  ss_tr_user_goal_points;
+drop table  ss_tr_user_badge;
+drop table ss_tr_user_reward;
+drop table  ss_tr_user_action;
+
+drop table  ss_ma_level;
+drop table  ss_ma_challenge;
+drop table  ss_ma_badge;
+drop table  ss_ma_reward;
+drop table  ss_ma_challenge;
+drop table  ss_ma_goal;
+drop table ss_ma_user;
+drop table ss_ma_user_type;
